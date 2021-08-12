4dc984e2928fa17860c5a0e1de80e7c686549f38
Lof.monday <nguyenlonghai21700@gmail.com>
2021-08-12 13:47:11 +0700

    Category drawer completed.

---
+++ src/components/ListItemLink/ListItemLink.jsx
@@ -0,0 +1,7 @@
+import { ListItem } from "@material-ui/core";
+
+const ListItemLink = props => {
+    return <ListItem button component="a" {...props} />;
+}
+
+export default ListItemLink
--- src/components/NavigationBar/NavigationCategories/NavigationCategories.jsx
+++ src/components/NavigationBar/NavigationCategories/NavigationCategories.jsx
@@ -1,10 +1,13 @@
 import { Box } from "@material-ui/core"
+import CategoriesDrawer from "./CategoriesDrawer"
 import CategoryDrawerToggler from "./CategoryDrawerToggler"
 
 const Categories = () => {
     return <Box display="flex" justifyContent="flex-start">
         {/* CATEGORIES TOGGLER */}
         <CategoryDrawerToggler />
+        {/* CATEGORIES DIV */}
+        <CategoriesDrawer />
     </Box >
 }
 
